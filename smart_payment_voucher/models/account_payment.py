ce57d9c2269d0140e3f25f49c09c2f2344fe3788
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-11 14:42:56 +0300

    Update account_payment.py

--- smart_payment_voucher/models/account_payment.py
+++ smart_payment_voucher/models/account_payment.py
@@ -1,9 +1,10 @@
-from odoo import models
 from odoo import models, fields, api
 from num2words import num2words
 
+
 class AccountPayment(models.Model):
     _inherit = "account.payment"
+
     amount_in_words = fields.Char(
         string="Amount In Words",
         compute="_compute_amount_in_words"
@@ -22,41 +23,41 @@ class AccountPayment(models.Model):
         return self.env.ref(
             "smart_payment_voucher.action_payment_voucher"
         ).report_action(self)
-        
+
     @api.depends("amount", "currency_id")
     def _compute_amount_in_words(self):
         for rec in self:
 
-         lang = rec.env.context.get("lang", "en_US")
+            lang = rec.env.user.lang or "en_US"
 
-         if lang.startswith("ar"):
-            amount_text = num2words(rec.amount, lang="ar")
+            if lang.startswith("ar"):
+                amount_text = num2words(rec.amount, lang="ar")
 
-            if rec.currency_id.name == "SAR":
-                currency_name = "ريال سعودي"
-            elif rec.currency_id.name == "USD":
-                currency_name = "دولار أمريكي"
-            elif rec.currency_id.name == "EUR":
-                currency_name = "يورو"
+                if rec.currency_id.name == "SAR":
+                    currency_name = "ريال سعودي"
+                elif rec.currency_id.name == "USD":
+                    currency_name = "دولار أمريكي"
+                elif rec.currency_id.name == "EUR":
+                    currency_name = "يورو"
+                else:
+                    currency_name = rec.currency_id.name
+
+                rec.amount_in_words = (
+                    f"{amount_text} {currency_name} فقط لا غير"
+                )
+
             else:
-                currency_name = rec.currency_id.name
+                amount_text = num2words(rec.amount, lang="en")
 
-            rec.amount_in_words = (
-                f"{amount_text} {currency_name} فقط لا غير"
-            )
+                if rec.currency_id.name == "SAR":
+                    currency_name = "Saudi Riyals"
+                elif rec.currency_id.name == "USD":
+                    currency_name = "US Dollars"
+                elif rec.currency_id.name == "EUR":
+                    currency_name = "Euros"
+                else:
+                    currency_name = rec.currency_id.name
 
-        else:
-            amount_text = num2words(rec.amount, lang="en")
-
-            if rec.currency_id.name == "SAR":
-                currency_name = "Saudi Riyals"
-            elif rec.currency_id.name == "USD":
-                currency_name = "US Dollars"
-            elif rec.currency_id.name == "EUR":
-                currency_name = "Euros"
-            else:
-                currency_name = rec.currency_id.name
-
-            rec.amount_in_words = (
-                f"{amount_text} {currency_name} Only"
-            )
+                rec.amount_in_words = (
+                    f"{amount_text} {currency_name} Only"
+                )
