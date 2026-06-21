12c346b3c036ab46149d1f1e71882c5f537b1f84
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-21 16:33:59 +0300

    Update account_payment.py

--- auto_fifo_reconciliation/models/account_payment.py
+++ auto_fifo_reconciliation/models/account_payment.py
@@ -8,7 +8,9 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     def action_post(self):
+        raise UserError("FIFO TEST")
         res = super().action_post()
+        
 
         for payment in self:
             payment._auto_reconcile_oldest_invoices()
