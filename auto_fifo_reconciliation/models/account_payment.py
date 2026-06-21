05f8e1c06b1bb851025e313935c06ee5ed4cdbd3
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-21 14:24:03 +0300

    Update account_payment.py

--- auto_fifo_reconciliation/models/account_payment.py
+++ auto_fifo_reconciliation/models/account_payment.py
@@ -1,4 +1,7 @@
+import logging
 from odoo import models
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountPayment(models.Model):
@@ -15,24 +18,24 @@ class AccountPayment(models.Model):
     def _auto_reconcile_oldest_invoices(self):
         self.ensure_one()
 
-        # للعملاء فقط
+        _logger.info("=== AUTO FIFO START %s ===", self.name)
+
         if self.partner_type != 'customer':
+            _logger.info("Not customer")
             return
 
-        # سطر الذمم المدينة الخاص بالدفعة
         payment_line = self.move_id.line_ids.filtered(
-            lambda l: (
-                l.account_id.account_type == 'asset_receivable'
-                and not l.reconciled
-            )
+            lambda l: l.account_id.account_type == 'asset_receivable'
+            and not l.reconciled
         )
+
+        _logger.info("Payment lines found: %s", len(payment_line))
 
         if not payment_line:
             return
 
         payment_line = payment_line[0]
 
-        # الفواتير المفتوحة مرتبة حسب تاريخ الاستحقاق
         invoices = self.env['account.move'].search([
             ('partner_id', '=', self.partner_id.id),
             ('move_type', '=', 'out_invoice'),
@@ -40,23 +43,26 @@ class AccountPayment(models.Model):
             ('payment_state', 'in', ['not_paid', 'partial']),
         ], order='invoice_date_due asc, id asc')
 
+        _logger.info("Invoices found: %s", len(invoices))
+
         for invoice in invoices:
 
+            _logger.info("Invoice: %s", invoice.name)
+
             invoice_line = invoice.line_ids.filtered(
-                lambda l: (
-                    l.account_id.account_type == 'asset_receivable'
-                    and not l.reconciled
-                )
+                lambda l: l.account_id.account_type == 'asset_receivable'
+                and not l.reconciled
             )
 
             if not invoice_line:
                 continue
 
             try:
+                _logger.info("Trying reconcile")
                 (payment_line + invoice_line[0]).reconcile()
-            except Exception:
-                continue
+                _logger.info("Reconcile success")
+            except Exception as e:
+                _logger.exception("Reconcile error: %s", e)
 
-            # إذا انتهى مبلغ الدفعة نتوقف
             if payment_line.reconciled:
                 break
