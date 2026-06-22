9c9c953d4c77d935c6e2b3083ad3b9bb36afb53d
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-22 17:41:38 +0300

    Update account_payment.py

--- auto_fifo_reconciliation/models/account_payment.py
+++ auto_fifo_reconciliation/models/account_payment.py
@@ -8,7 +8,6 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     def action_post(self):
-
         res = super().action_post()
 
         for payment in self:
@@ -20,18 +19,22 @@ class AccountPayment(models.Model):
         return res
 
     def _auto_reconcile_oldest_invoices(self):
+        self.ensure_one()
+
         if not self.partner_id:
-    return
+            return
 
         _logger.info("=== AUTO FIFO START %s ===", self.name)
 
-        if self.partner_type != 'customer':
-            _logger.info("Not customer")
+        if self.payment_type != "inbound" or self.partner_type != "customer":
+            _logger.info("Not inbound customer payment")
             return
 
         payment_line = self.move_id.line_ids.filtered(
-            lambda l: l.account_id.account_type == 'asset_receivable'
-            and not l.reconciled
+            lambda l: (
+                l.account_id.account_type == "asset_receivable"
+                and not l.reconciled
+            )
         )
 
         _logger.info("Payment lines found: %s", len(payment_line))
@@ -39,15 +42,24 @@ class AccountPayment(models.Model):
         if not payment_line:
             return
 
+        if len(payment_line) != 1:
+            _logger.warning(
+                "Expected 1 receivable payment line, found %s",
+                len(payment_line),
+            )
+
         payment_line = payment_line[0]
 
-        receivable_lines = self.env['account.move.line'].search([
-            ('partner_id', '=', self.partner_id.id),
-            ('account_id.account_type', '=', 'asset_receivable'),
-            ('parent_state', '=', 'posted'),
-            ('reconciled', '=', False),
-            ('balance', '>', 0),
-        ], order='date asc, id asc')
+        receivable_lines = self.env["account.move.line"].search(
+            [
+                ("partner_id", "=", self.partner_id.id),
+                ("account_id.account_type", "=", "asset_receivable"),
+                ("parent_state", "=", "posted"),
+                ("reconciled", "=", False),
+                ("balance", ">", 0),
+            ],
+            order="date asc, id asc",
+        )
 
         _logger.info("=== RECEIVABLE LINES ===")
 
@@ -79,6 +91,7 @@ class AccountPayment(models.Model):
                 _logger.info("Trying reconcile")
                 (payment_line + line).reconcile()
                 _logger.info("Reconcile success")
+
             except Exception:
                 _logger.exception("Reconcile error")
                 raise
