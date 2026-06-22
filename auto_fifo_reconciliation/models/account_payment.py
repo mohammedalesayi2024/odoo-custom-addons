d5f4977e0abb643efc8cf48209334a4d63e5b894
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-22 13:37:13 +0300

    Update account_payment.py

--- auto_fifo_reconciliation/models/account_payment.py
+++ auto_fifo_reconciliation/models/account_payment.py
@@ -8,9 +8,8 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     def action_post(self):
-        
+
         res = super().action_post()
-        
 
         for payment in self:
             payment._auto_reconcile_oldest_invoices()
@@ -38,33 +37,49 @@ class AccountPayment(models.Model):
 
         payment_line = payment_line[0]
 
-        invoices = self.env['account.move'].search([
+        receivable_lines = self.env['account.move.line'].search([
             ('partner_id', '=', self.partner_id.id),
-            ('move_type', '=', 'out_invoice'),
-            ('state', '=', 'posted'),
-            ('payment_state', 'in', ['not_paid', 'partial']),
-        ], order='invoice_date_due asc, id asc')
+            ('account_id.account_type', '=', 'asset_receivable'),
+            ('parent_state', '=', 'posted'),
+            ('reconciled', '=', False),
+            ('balance', '>', 0),
+        ], order='date asc, id asc')
 
-        _logger.info("Invoices found: %s", len(invoices))
+        _logger.info("=== RECEIVABLE LINES ===")
 
-        for invoice in invoices:
-
-            _logger.info("Invoice: %s", invoice.name)
-
-            invoice_line = invoice.line_ids.filtered(
-                lambda l: l.account_id.account_type == 'asset_receivable'
-                and not l.reconciled
+        for line in receivable_lines:
+            _logger.info(
+                "MOVE=%s | PARTNER=%s | BALANCE=%s | RESIDUAL=%s | ID=%s",
+                line.move_id.name,
+                line.partner_id.name,
+                line.balance,
+                line.amount_residual,
+                line.id,
             )
 
-            if not invoice_line:
+        _logger.info("Receivable lines found: %s", len(receivable_lines))
+
+        for line in receivable_lines:
+
+            if line.id == payment_line.id:
                 continue
+
+            _logger.info(
+                "Processing line %s | Move=%s | Residual=%s",
+                line.id,
+                line.move_id.name,
+                line.amount_residual,
+            )
 
             try:
                 _logger.info("Trying reconcile")
-                (payment_line + invoice_line[0]).reconcile()
+                (payment_line + line).reconcile()
                 _logger.info("Reconcile success")
             except Exception as e:
                 _logger.exception("Reconcile error: %s", e)
 
             if payment_line.reconciled:
+                _logger.info("Payment fully reconciled")
                 break
+
+        _logger.info("=== AUTO FIFO END %s ===", self.name)
