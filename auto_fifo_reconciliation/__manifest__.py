ef0fde915f396f69ca128f6fa04813f749518ff2
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-22 15:27:52 +0300

    Update __manifest__.py

--- auto_fifo_reconciliation/__manifest__.py
+++ auto_fifo_reconciliation/__manifest__.py
@@ -2,12 +2,12 @@
     "name": "Auto FIFO Reconciliation",
     "version": "19.0.1.0.1",
     "category": "Accounting",
+    "author": "Mohammed",
+    "license": "LGPL-3",
     "depends": ["account"],
-
     "data": [
         "views/res_partner_views.xml",
     ],
-
     "installable": True,
     "application": False,
 }
