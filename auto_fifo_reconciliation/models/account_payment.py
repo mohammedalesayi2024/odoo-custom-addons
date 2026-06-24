8edc5b68282179a0bc9f7611e7ffd910a979be4d
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-24 11:18:16 +0300

    upgrade

--- auto_fifo_reconciliation/models/account_payment.py
+++ auto_fifo_reconciliation/models/account_payment.py
@@ -50,16 +50,48 @@ class AccountPayment(models.Model):
 
         payment_line = payment_line[0]
 
-        receivable_lines = self.env["account.move.line"].search(
-            [
-                ("partner_id", "=", self.partner_id.id),
-                ("account_id.account_type", "=", "asset_receivable"),
-                ("parent_state", "=", "posted"),
-                ("reconciled", "=", False),
-                ("balance", ">", 0),
-            ],
-            order="date asc, id asc",
-        )
+        method = self.partner_id.payment_allocation_method
+
+        domain = [
+            ("partner_id", "=", self.partner_id.id),
+            ("account_id.account_type", "=", "asset_receivable"),
+            ("parent_state", "=", "posted"),
+            ("reconciled", "=", False),
+            ("balance", ">", 0),
+        ]
+
+        if method == "date":
+
+            receivable_lines = self.env["account.move.line"].search(
+                domain,
+                order="date asc, id asc",
+            )
+
+        else:
+
+            all_lines = self.env["account.move.line"].search(domain)
+
+            non_invoice_lines = all_lines.filtered(
+                lambda l: l.move_id.move_type != "out_invoice"
+            ).sorted(
+                key=lambda l: (
+                    l.date or l.move_id.date,
+                    l.id,
+                )
+            )
+
+            invoice_lines = all_lines.filtered(
+                lambda l: l.move_id.move_type == "out_invoice"
+            ).sorted(
+                key=lambda l: (
+                    l.move_id.invoice_date_due
+                    or l.move_id.invoice_date
+                    or l.date,
+                    l.id,
+                )
+            )
+
+            receivable_lines = non_invoice_lines + invoice_lines
 
         _logger.info("=== RECEIVABLE LINES ===")
 
