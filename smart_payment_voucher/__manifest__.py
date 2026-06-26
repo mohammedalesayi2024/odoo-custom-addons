54e216328bc43c68cea1a4cacd69f1472e3a2f92
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-26 23:04:04 +0300

    update

--- smart_payment_voucher/__manifest__.py
+++ smart_payment_voucher/__manifest__.py
@@ -25,10 +25,7 @@ Features:
 
         "views/res_company_views.xml",
         "views/account_payment_views.xml",
-        "views/res_config_settings_views.xml",
-
-        "reports/report_paperformat.xml",
-        
+        "views/res_config_settings_views.xml",    
         "reports/receipt_voucher.xml",
         "reports/payment_voucher.xml",
         "reports/receipt_template.xml",
