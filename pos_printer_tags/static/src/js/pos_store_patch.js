8584b0fdf90da4f9a2cc31e46f8947d5ec5e475e
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-06-28 11:00:33 +0300

    Update pos_store_patch.js

--- pos_printer_tags/static/src/js/pos_store_patch.js
+++ pos_printer_tags/static/src/js/pos_store_patch.js
@@ -6,13 +6,38 @@ import { PosStore } from "@point_of_sale/app/services/pos_store";
 patch(PosStore.prototype, {
 
     filterChangeByTags(tagIds, currentOrderChange) {
-        console.log("POS Printer Tags", tagIds);
+
+        console.log("filterChangeByTags Loaded");
+
+        const matchesTags = (change) => {
+            const product = this.models["product.product"].get(change.product_id);
+
+            const productTags = product.product_tag_ids || [];
+
+            return productTags.some(tagId => tagIds.includes(tagId));
+        };
+
+        const filterChanges = (changes) => {
+
+            const validComboUuids = new Set(
+                changes
+                    .filter(change => change.combo_parent_uuid && matchesTags(change))
+                    .map(change => change.combo_parent_uuid)
+            );
+
+            return changes.filter(change =>
+                (change.isCombo && validComboUuids.has(change.uuid)) ||
+                (!change.isCombo && matchesTags(change))
+            );
+
+        };
 
         return {
-            new: [],
-            cancelled: [],
-            noteUpdate: [],
+            new: filterChanges(currentOrderChange.new),
+            cancelled: filterChanges(currentOrderChange.cancelled),
+            noteUpdate: filterChanges(currentOrderChange.noteUpdate),
         };
+
     },
 
 });
