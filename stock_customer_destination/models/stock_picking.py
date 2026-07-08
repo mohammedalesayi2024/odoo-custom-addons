258e5d7d7efcdcd86a2363cd80794fc7ac10ebfe
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-08 11:52:02 +0300

    Update stock_picking.py

--- stock_customer_destination/models/stock_picking.py
+++ stock_customer_destination/models/stock_picking.py
@@ -1,4 +1,7 @@
 from odoo import api, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class StockPicking(models.Model):
@@ -6,49 +9,19 @@ class StockPicking(models.Model):
 
     @api.onchange("partner_id")
     def _onchange_partner_id_set_destination_location(self):
-        """
-        عند اختيار العميل في التحويل الداخلي يتم تحديث
-        موقع الوجهة مباشرة إلى موقع العميل.
-        """
+        _logger.warning("===== ONCHANGE PARTNER =====")
+
         for picking in self:
+            _logger.warning("Partner: %s", picking.partner_id.name)
+
             if (
                 picking.picking_type_id
                 and picking.picking_type_id.code == "internal"
                 and picking.partner_id
                 and picking.partner_id.property_stock_customer
             ):
-                picking.location_dest_id = (
-                    picking.partner_id.property_stock_customer
+                picking.location_dest_id = picking.partner_id.property_stock_customer
+                _logger.warning(
+                    "Destination: %s",
+                    picking.location_dest_id.display_name,
                 )
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        records = super().create(vals_list)
-
-        for picking in records:
-            if (
-                picking.picking_type_id.code == "internal"
-                and picking.partner_id
-                and picking.partner_id.property_stock_customer
-            ):
-                picking.location_dest_id = (
-                    picking.partner_id.property_stock_customer
-                )
-
-        return records
-
-    def write(self, vals):
-        res = super().write(vals)
-
-        if "partner_id" in vals:
-            for picking in self:
-                if (
-                    picking.picking_type_id.code == "internal"
-                    and picking.partner_id
-                    and picking.partner_id.property_stock_customer
-                ):
-                    picking.location_dest_id = (
-                        picking.partner_id.property_stock_customer
-                    )
-
-        return res
