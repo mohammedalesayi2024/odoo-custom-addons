959bf0e70be69465b405e488d7d2639022d824a9
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-08 12:20:44 +0300

    Update stock_picking.py

--- stock_customer_destination/models/stock_picking.py
+++ stock_customer_destination/models/stock_picking.py
@@ -6,6 +6,10 @@ class StockPicking(models.Model):
 
     @api.onchange("partner_id")
     def _onchange_partner_id_set_destination_location(self):
+        """
+        عند اختيار العميل في التحويل الداخلي يتم تحديث
+        موقع الوجهة مباشرة إلى موقع العميل.
+        """
         for picking in self:
             if (
                 picking.picking_type_id
@@ -13,6 +17,38 @@ class StockPicking(models.Model):
                 and picking.partner_id
                 and picking.partner_id.property_stock_customer
             ):
-                picking.update({
-                    "location_dest_id": picking.partner_id.property_stock_customer.id,
-                })
+                picking.location_dest_id = (
+                    picking.partner_id.property_stock_customer
+                )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+
+        for picking in records:
+            if (
+                picking.picking_type_id.code == "internal"
+                and picking.partner_id
+                and picking.partner_id.property_stock_customer
+            ):
+                picking.location_dest_id = (
+                    picking.partner_id.property_stock_customer
+                )
+
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+
+        if "partner_id" in vals:
+            for picking in self:
+                if (
+                    picking.picking_type_id.code == "internal"
+                    and picking.partner_id
+                    and picking.partner_id.property_stock_customer
+                ):
+                    picking.location_dest_id = (
+                        picking.partner_id.property_stock_customer
+                    )
+
+        return res
