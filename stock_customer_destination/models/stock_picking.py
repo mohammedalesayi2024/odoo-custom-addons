e1421aeb5d21134ba03f5e673e929c5a13d9b90b
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-08 12:09:55 +0300

    Update stock_picking.py

--- stock_customer_destination/models/stock_picking.py
+++ stock_customer_destination/models/stock_picking.py
@@ -1,9 +1,4 @@
 from odoo import api, models
-import logging
-
-_logger = logging.getLogger(__name__)
-
-_logger.warning("######### STOCK PICKING FILE LOADED #########")
 
 
 class StockPicking(models.Model):
@@ -11,19 +6,13 @@ class StockPicking(models.Model):
 
     @api.onchange("partner_id")
     def _onchange_partner_id_set_destination_location(self):
-        _logger.warning("===== ONCHANGE PARTNER =====")
-
         for picking in self:
-            _logger.warning("Partner: %s", picking.partner_id.name)
-
             if (
                 picking.picking_type_id
                 and picking.picking_type_id.code == "internal"
                 and picking.partner_id
                 and picking.partner_id.property_stock_customer
             ):
-                picking.location_dest_id = picking.partner_id.property_stock_customer
-                _logger.warning(
-                    "Destination: %s",
-                    picking.location_dest_id.display_name,
-                )
+                picking.update({
+                    "location_dest_id": picking.partner_id.property_stock_customer.id,
+                })
