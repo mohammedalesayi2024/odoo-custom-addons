69a04b7c1ae8aa588a5fb6c8454b4d2cfe17ddef
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-12 01:43:09 +0300

    Update model_tracker.py

--- advance_audit/services/model_tracker.py
+++ advance_audit/services/model_tracker.py
@@ -1,8 +1,24 @@
-from odoo import models
+from odoo import api, models
 
 
 class Base(models.AbstractModel):
     _inherit = "base"
 
+    @api.model_create_multi
     def create(self, vals_list):
-        return super().create(vals_list)
+        records = super().create(vals_list)
+
+        # Ignore Advance Audit models
+        if self._name.startswith("advance.audit"):
+            return records
+
+        audit = self.env["advance.audit.log.service"]
+
+        for record in records:
+            audit.log_event(
+                record=record,
+                event_type="create",
+                description="Record created",
+            )
+
+        return records
