8a9667a3f4bf5edbf6b8d54f33fd8e642a95ab3e
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-27 01:31:34 +0300

    Update stock_move_line.py

--- stock_move_invoice_reference/models/stock_move_line.py
+++ stock_move_invoice_reference/models/stock_move_line.py
@@ -1,4 +1,7 @@
 from odoo import api, fields, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class StockMoveLine(models.Model):
@@ -16,4 +19,5 @@ class StockMoveLine(models.Model):
     @api.depends("move_id")
     def _compute_invoice_reference(self):
         for line in self:
+            _logger.warning("FIELDS: %s", list(line.move_id._fields.keys()))
             line.invoice_reference_id = False
