b3877189122207c8f1f10ae66587a1b239fee396
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-27 02:18:01 +0300

    Update stock_move_line.py

--- stock_move_invoice_reference/models/stock_move_line.py
+++ stock_move_invoice_reference/models/stock_move_line.py
@@ -1,34 +1,51 @@
 from odoo import api, fields, models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     invoice_reference_id = fields.Many2one(
-        comodel_name="account.move",
+        "account.move",
         string="Invoice Reference",
         compute="_compute_invoice_reference",
-        readonly=True,
         store=False,
+        readonly=True,
     )
 
-    @api.depends("move_id.sale_line_id")
+    @api.depends("move_id.sale_line_id", "move_id.purchase_line_id")
     def _compute_invoice_reference(self):
         for line in self:
             line.invoice_reference_id = False
 
-            sale_line = line.move_id.sale_line_id
-            if not sale_line:
+            # Sales
+            if line.move_id.sale_line_id:
+                order = line.move_id.sale_line_id.order_id
+                invoices = order.invoice_ids.filtered(
+                    lambda m: m.state == "posted"
+                    and m.move_type in ("out_invoice", "out_refund")
+                )
+                if invoices:
+                    line.invoice_reference_id = invoices[0]
                 continue
 
-            order = sale_line.order_id
-            if not order:
+            # Purchase
+            purchase_line = line.move_id.purchase_line_id
+            if not purchase_line:
+                _logger.warning("NO PURCHASE LINE")
                 continue
 
-            invoices = order.invoice_ids.filtered(
-                lambda m: m.state == "posted"
-                and m.move_type in ("out_invoice", "out_refund")
-            )
+            purchase = purchase_line.order_id
 
-            if invoices:
-                line.invoice_reference_id = invoices[0]
+            _logger.warning("PURCHASE: %s", purchase.name)
+            _logger.warning("INVOICE IDS: %s", purchase.invoice_ids.ids)
+
+            for inv in purchase.invoice_ids:
+                _logger.warning(
+                    "Vendor Bill: %s state=%s type=%s",
+                    inv.name,
+                    inv.state,
+                    inv.move_type,
+                )
