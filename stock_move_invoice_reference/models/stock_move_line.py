ef77748816636a9dc6f3196df3a459c797165c8e
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-27 01:43:06 +0300

    Update stock_move_line.py

--- stock_move_invoice_reference/models/stock_move_line.py
+++ stock_move_invoice_reference/models/stock_move_line.py
@@ -13,7 +13,23 @@ class StockMoveLine(models.Model):
         index=True,
     )
 
-    @api.depends("move_id.account_move_id")
+    @api.depends("move_id.sale_line_id")
     def _compute_invoice_reference(self):
         for line in self:
-            line.invoice_reference_id = line.move_id.account_move_id
+            line.invoice_reference_id = False
+
+            sale_line = line.move_id.sale_line_id
+            if not sale_line:
+                continue
+
+            order = sale_line.order_id
+            if not order:
+                continue
+
+            invoices = order.invoice_ids.filtered(
+                lambda m: m.state == "posted"
+                and m.move_type in ("out_invoice", "out_refund")
+            )
+
+            if invoices:
+                line.invoice_reference_id = invoices[0]
