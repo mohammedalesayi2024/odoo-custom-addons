cf2c73bf03ff84a5aaa864011113e34aaa5b8833
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-27 01:13:34 +0300

    Update stock_move_line.py

--- stock_move_invoice_reference/models/stock_move_line.py
+++ stock_move_invoice_reference/models/stock_move_line.py
@@ -1,14 +1,19 @@
-# Stock Move model extension will be implemented here.
-from odoo import fields, models
+from odoo import api, fields, models
 
 
-class StockMove(models.Model):
+class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     invoice_reference_id = fields.Many2one(
         comodel_name="account.move",
         string="Invoice Reference",
+        compute="_compute_invoice_reference",
+        store=True,
         readonly=True,
-        copy=False,
         index=True,
     )
+
+    @api.depends("move_id")
+    def _compute_invoice_reference(self):
+        for line in self:
+            line.invoice_reference_id = False
