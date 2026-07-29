f51be2367553d5fbfc59949a19c4329020975a37
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-07-29 09:25:44 +0300

    U

--- asset_custom_field/models/account_asset.py
+++ asset_custom_field/models/account_asset.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class AccountAsset(models.Model):
     _inherit = "account.asset"
 
-    asset_note = fields.Char(string="Asset Note")
+    Asset_Location = fields.Char(string="Asset Location")
