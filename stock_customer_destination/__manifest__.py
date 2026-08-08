861f9da23356a9de16f4060ff07a1c5e45e6ee3d
moh_alesayi <mohammedalesayi2024@gmail.com>
2026-08-08 11:55:14 +0300

    Update __manifest__.py

--- stock_customer_destination/__manifest__.py
+++ stock_customer_destination/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "19.0.1.0.0",
     "category": "Inventory",
     "summary": "Automatically assign customer's stock location for internal transfers",
-    "author": "Mohammed",
+    "author": "Mohammed Alesayi",
     "license": "LGPL-3",
     "depends": ["stock"],
     "data": [],
